980d52b57c0c618b6fe28b0c7cd8ba24e40c2c64
KianAnakNiLord <161108066+KianAnakNiLord@users.noreply.github.com>
2024-05-24 03:03:26 +0800

    TaskManagerApp.java

--- TaskManagerApp.java
+++ TaskManagerApp.java
@@ -2,7 +2,7 @@ import javax.swing.SwingUtilities;
 
 public class TaskManagerApp {
     public static void main(String[] args) {
-        // Ensure database tables are created
+        
         DatabaseUtil.createTables();
 
         SwingUtilities.invokeLater(() -> {
